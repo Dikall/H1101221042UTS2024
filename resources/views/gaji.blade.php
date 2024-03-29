53504949092e3fa857f450c07cee3ebc86bf4930
peru299 <hashitaputeri29@gmail.com>
2024-03-29 12:51:09 +0700

    Gaji

--- resources/views/gaji.blade.php
+++ resources/views/gaji.blade.php
@@ -114,145 +114,60 @@
             }
         </style>
     </head>
-    <body>
+        <body>
         <div class="row">
             <div class="col-75">
                 <div class="container">
                     <form action="/action_page.php">
                         <div class="row">
                             <div class="col-50">
-                                <h3>Nama Pegawai</h3>
-                                <label for="fname"
-                                    ><i class="fa fa-user"></i> Full Name</label
-                                >
-                                <input
-                                    type="text"
-                                    id="fname"
-                                    name="firstname"
-                                    placeholder="John M. Doe"
-                                />
-                                <label for="fname"
-                                    ><i class="fa fa-user"></i> Full Name</label
-                                >
-                                <input
-                                    type="text"
-                                    id="fname"
-                                    name="firstname"
-                                    placeholder="John M. Doe"
-                                />
-                                <label for="fname"
-                                    ><i class="fa fa-user"></i> Full Name</label
-                                >
-                                <input
-                                    type="text"
-                                    id="fname"
-                                    name="firstname"
-                                    placeholder="John M. Doe"
-                                />
-                                <label for="fname"
-                                    ><i class="fa fa-user"></i> Full Name</label
-                                >
-                                <input
-                                    type="text"
-                                    id="fname"
-                                    name="firstname"
-                                    placeholder="John M. Doe"
-                                />
-
+                                <h3 style="text-align: center;">Form Gaji Pegawai</h3>
                                 <div class="row">
-                                    <div class="col-50">
-                                        <label for="state">State</label>
+                                    <div class="col-25">
+                                        <label for="fname">
+                                            <i class="fa fa-user"></i> Nama Pegawai</label>
                                         <input
                                             type="text"
-                                            id="state"
-                                            name="state"
-                                            placeholder="NY"
+                                            id="fname"
+                                            name="firstname"
+                                            placeholder="M. Radikal"
                                         />
                                     </div>
-                                    <div class="col-50">
-                                        <label for="zip">Zip</label>
+                                    <div class="col-25">
+                                        <label for="fname">
+                                            <i class="fas fa-address-card"></i> Jabatan</label>
                                         <input
                                             type="text"
-                                            id="zip"
-                                            name="zip"
-                                            placeholder="10001"
+                                            id="fname"
+                                            name="firstname"
+                                            placeholder="Manager"
                                         />
                                     </div>
+                                    <div class="col-25">
+                                        <label for="fname">
+                                            <i class="fas fa-money-bill-wave"></i> Gaji</label>
+                                        <input
+                                            type="text"
+                                            id="fname"
+                                            name="firstname"
+                                            placeholder="20.000.000"
+                                        />
+                                    </div>
+                                    <div class="col-25">
+                                        <label for="fname">
+                                            <i class="fas fa-user-graduate"></i> Pendidikan</label>
+                                        <input
+                                            type="text"
+                                            id="fname"
+                                            name="firstname"
+                                            placeholder="Profesor"
+                                        />
+                                    </div>
+                                    
                                 </div>
                             </div>
-
-                            <div class="col-50">
-                                <h3>Gaji</h3>
-                                <label for="fname">Accepted Cards</label>
-                                <div class="icon-container">
-                                    <i
-                                        class="fa fa-cc-visa"
-                                        style="color: navy"
-                                    ></i>
-                                    <i
-                                        class="fa fa-cc-amex"
-                                        style="color: blue"
-                                    ></i>
-                                    <i
-                                        class="fa fa-cc-mastercard"
-                                        style="color: red"
-                                    ></i>
-                                    <i
-                                        class="fa fa-cc-discover"
-                                        style="color: orange"
-                                    ></i>
-                                </div>
-                                <label for="cname">Name on Card</label>
-                                <input
-                                    type="text"
-                                    id="cname"
-                                    name="cardname"
-                                    placeholder="John More Doe"
-                                />
-                                <label for="ccnum">Credit card number</label>
-                                <input
-                                    type="text"
-                                    id="ccnum"
-                                    name="cardnumber"
-                                    placeholder="1111-2222-3333-4444"
-                                />
-                                <label for="expmonth">Exp Month</label>
-                                <input
-                                    type="text"
-                                    id="expmonth"
-                                    name="expmonth"
-                                    placeholder="September"
-                                />
-                                <div class="row">
-                                    <div class="col-50">
-                                        <label for="expyear">Exp Year</label>
-                                        <input
-                                            type="text"
-                                            id="expyear"
-                                            name="expyear"
-                                            placeholder="2018"
-                                        />
-                                    </div>
-                                    <div class="col-50">
-                                        <label for="cvv">CVV</label>
-                                        <input
-                                            type="text"
-                                            id="cvv"
-                                            name="cvv"
-                                            placeholder="352"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked="checked"
-                                name="sameadr"
-                            />
-                            Shipping address same as billing
-                        </label>
                         <input
                             type="submit"
                             value="Continue to checkout"
--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,6 @@ use App\Http\Controllers\Home;
 use App\Http\Controllers\Jabatan;
 use App\Http\Controllers\Presensi;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 
 Route::get('gaji', [Gaji::class, 'index']);
 Route::get('home', [Home::class, 'index']);
